b92a3efeb53334f7837ee5ee93d1e2037356c9df
Jessica Gordon <ketchesj@gmail.com>
2021-11-09 16:56:49 +0000

    Step 8: Exercise 1

---
+++ exercises_directory.rb
@@ -0,0 +1,42 @@
+# Villains Academy Directory
+
+# Asking user for list of students
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # Get the first name
+  name = gets.chomp
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    # Add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # Get another name from the user
+    name = gets.chomp
+  end
+  # Return the array of students
+  students
+end  
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students) 
+  puts "Overall, we have #{students.count} great students"
+end
+
+# Call methods to print student directory
+students = input_students
+print_header
+print(students)
+print_footer(students)
