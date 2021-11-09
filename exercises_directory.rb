5798d403ca2817c95605b4603552cadfea988bc2
Jessica Gordon <ketchesj@gmail.com>
2021-11-09 17:09:53 +0000

    Step 8: Exercise 5 - Our code only works with the student name and cohort. Add more information: hobbies, country of birth, height, etc.

--- exercises_directory.rb
+++ exercises_directory.rb
@@ -2,19 +2,23 @@
 
 # Asking user for list of students
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the details of each student (name, hobby)"
   puts "To finish, just hit return twice"
   # Create an empty array
   students = []
-  # Get the first name
-  name = gets.chomp
   # While the name is not empty, repeat this code
-  while !name.empty? do
-    # Add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # Get another name from the user
+  while true do
+    # Get a name from the user
+    puts "name:"
     name = gets.chomp
+    break if name.empty?
+
+    # Get their favourite hobby
+    puts "hobby:"
+    hobby = gets.chomp    
+    # Add the student hash to the array
+    students << {name: name, cohort: :november, hobby: hobby}
+    puts "Now we have #{students.count} students"
   end
   # Return the array of students
   students
@@ -28,7 +32,7 @@ end
 def print(students)
   count = 0
   while count < students.count do
-    puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort). In their spare time they love to #{students[count][:hobby]}!"
     count += 1
   end
 end
