d8fff6ca3536e8b45d65ff303684f0f44408527f
Jessica Gordon <ketchesj@gmail.com>
2021-11-11 19:43:47 +0000

    Added the option to use command line arguments to pre-load student list before activitating the interactive menu

--- directory.rb
+++ directory.rb
@@ -6,14 +6,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # Get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # While the name is not empty, repeat this code
   while !name.empty? do
     # Add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # Get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end  
 
@@ -58,13 +58,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
 end
 
 def process(selection)
@@ -87,9 +99,10 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 # Call the interactive menu at the start of the program
+try_load_students
 interactive_menu
